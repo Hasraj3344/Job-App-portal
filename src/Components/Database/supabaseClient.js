1e333f0205af6c4ee44dfa76a4f33e61029333ba
Neelam Haswanth Rajesh <hasraj@Neelams-MacBook-Pro.local>
2025-04-30 14:44:29 -0400

    changed supabase url and key calls

--- src/Components/Database/supabaseClient.js
+++ src/Components/Database/supabaseClient.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Load environment variables from Render environment
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseUrl = process.env.supabaseUrl;
+const supabaseKey = process.env.supabaseKey;
 
 // Create and export the Supabase client instance
 export const supabase = createClient(supabaseUrl, supabaseKey);
